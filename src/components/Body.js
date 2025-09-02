b560d2abfb6f5b7bc551813901b5d0f965ce07c5
Abdul291095 <abdul291095@gmail.com>
2025-09-02 15:39:57 +0530

    ep-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,71 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
-  return (
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0330385&lng=80.20528949999999&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+    );
+
+    const json = await data.json();
+
+    const restaurantCards = json?.data?.cards?.slice(3) || [];
+
+    setListOfRestaurant(restaurantCards);
+    setFilteredRestaurant(restaurantCards);
+  };
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      {/* <div className="search">Search</div> */}
-      <button
-        className="filter-btn"
-        onClick={() => {
-          const filteredRestaurant = listOfRestaurant.filter(
-            (res) => res.card.card.info.avgRating >= 4.5
-          );
-          setListOfRestaurant(filteredRestaurant);
-        }}
-      >
-        Top Rated Restaurants
-      </button>
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+              const filteredRestaurant = listOfRestaurant.filter((res) =>
+                res.card.card.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurant = listOfRestaurant.filter(
+              (res) => res.card.card.info.avgRating >= 4.5
+            );
+            setListOfRestaurant(filteredRestaurant);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {listOfRestaurant.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard
             key={restaurant?.card?.card?.info?.id}
             resData={restaurant}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,33 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
-const Header = () => (
-  <div className="header">
-    <div className="logo-container">
-      <img className="logo" src={LOGO_URL} />
-    </div>
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-);
+const Header = () => {
+  const [loginBtn, setLoginBtn] = useState("Login");
 
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              loginBtn === "Login"
+                ? setLoginBtn("Logout")
+                : setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Header;
