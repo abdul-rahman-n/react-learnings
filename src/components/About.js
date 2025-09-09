1431b7d4285afb6bdf4d625177eff06711289630
Abdul291095 <abdul291095@gmail.com>
2025-09-09 12:58:45 +0530

    ep-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,24 @@
-const About = () => {
-  return (
-    <div>
-      <h1>This is About Page</h1>
-      <h2>some more about us content</h2>
-    </div>
-  );
-};
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>This is About Page</h1>
+        <h2>some more about us content</h2>
+        <UserClass name={"FIRST"} location={"Pallavaram (Class)"} />
+      </div>
+    );
+  }
+}
 
 export default About;
